99894ae59e19740feb7992e880b2a74a376a0ddc
Carlos <dens.walker22@gmail.com>
2021-04-26 15:04:56 +0200

    1

---
+++ timer/src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import Button from './components/button'
+import Button from './components/timer'
+import Button from './components/time-input'
+
+
+function App() {
+
+export default App
---
+++ timer/src/components/timer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Timer({ value }) {
+    return <div onClick={onClick}></div>
+}
+
+export default Timer
